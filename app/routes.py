3f00d817e99885e55c486f4fa5eeb2fde75a93df
Varetoo <alvaromi2005@gmail.com>
2025-02-19 22:45:47 +0100

    Agregar estructura básica de la aplicación Flask con configuración, rutas y formularios de inicio de sesión

---
+++ app/routes.py
@@ -0,0 +1,33 @@
+from flask import render_template, redirect, flash, url_for
+from app import app
+from app.forms import LoginForm
+
+@app.route('/')
+@app.route('/index')
+def index():
+    user = {'username': 'Warell'}
+    posts = [
+        {
+            'author': {'username': 'Warell'},
+            'body': 'Un día fantástico para programar'
+        },
+        {
+            'author': {'username': 'Lesomio'},
+            'body': 'Me encanta jugar al rocket leage'
+        },
+        {
+            'author': {'username': 'Totonio'},
+            'body': 'No me gusta el futbol'
+        }
+    ]
+    return render_template('index.html', title='Home Page', user=user, posts=posts)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign in', form=form)    
+
