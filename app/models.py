83b9b56e8eeeae06f4cf729a1b729ccfd5d3d05b
Varetoo <alvaromi2005@gmail.com>
2025-02-21 11:44:11 +0100

    Agregar soporte para Flask-Login en el modelo de usuario

--- app/models.py
+++ app/models.py
@@ -6,7 +6,7 @@ from app import db
 from werkzeug.security import generate_password_hash ,check_password_hash
 from flask_login import UserMixin
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
 
     username: so.Mapped[str] = so.mapped_column(sa.String(64), index=True,
