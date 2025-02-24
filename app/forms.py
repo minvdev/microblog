181b128767d2230f72cb75e1884ad832daa7f4b9
Varetoo <alvaromi2005@gmail.com>
2025-02-24 13:01:18 +0100

    validate username function in edit profile form

--- app/forms.py
+++ app/forms.py
@@ -32,6 +32,17 @@ class RegistrationForm(FlaskForm):
             raise ValidationError('Please use a different email adress.')
 
 class EditProfileForm(FlaskForm):
+    def __init__(self, original_username, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.original_username = original_username
+    
     username = StringField('Username', validators=[DataRequired()])
     about_me = TextAreaField('About me', validators=[Length(min=0, max=140)])
     submit = SubmitField('Submit')
+    
+    def validate_username(self, username):
+        if username.data != self.original_username:
+            user = db.session.scalar(
+                sa.select(User).where(User.username == username.data))
+            if user is not None:
+                raise ValidationError('Please use a different username.')
