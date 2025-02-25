b52b592dc9f62b08dad78c6bd2f669f47fa82703
Varetoo <alvaromi2005@gmail.com>
2025-02-25 20:14:42 +0100

    posts syntax fix /routes

--- app/routes.py
+++ app/routes.py
@@ -26,9 +26,9 @@ def index():
         db.session.commit()
         flash('Your post is now live!')
         return redirect(url_for('index'))
-    post = db.session.scalars(current_user.following_posts()).all()
+    posts = db.session.scalars(current_user.following_posts()).all()
     return render_template('index.html', title='Home Page', form=form,
-                           post=post)
+                           posts=posts)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
