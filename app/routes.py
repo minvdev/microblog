d841310f7ad5ff216bc8f37908067a2811c19b66
Varetoo <alvaromi2005@gmail.com>
2025-02-25 21:00:05 +0100

    Pagination for showing posts in index and explore view functions /routes

--- app/routes.py
+++ app/routes.py
@@ -26,9 +26,12 @@ def index():
         db.session.commit()
         flash('Your post is now live!')
         return redirect(url_for('index'))
+    page = request.args.get('page', 1, type=int)
     posts = db.session.scalars(current_user.following_posts()).all()
+    posts = db.paginate(current_user.following_posts(), page=page,
+                        per_page=app.config['POSTS_PER_PAGE'], error_out=False)
     return render_template('index.html', title='Home Page', form=form,
-                           posts=posts)
+                           posts=posts.items)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -114,7 +117,6 @@ def follow(username):
     else:
         return redirect(url_for('index'))
 
-
 @app.route('/unfollow/<username>', methods=['POST'])
 @login_required
 def unfollow(username):
@@ -138,6 +140,8 @@ def unfollow(username):
 @app.route('/explore')
 @login_required
 def explore():
+    page = request.args.get('page', 1, type=int)
     query = sa.select(Post).order_by(Post.timestamp.desc())
-    posts = db.session.scalars(query).all()
-    return render_template("index.html", title='Explore', posts=posts)
+    posts = db.paginate(query, page=page,
+                        per_page=app.config['POSTS_PER_PAGE'], error_out=False)
+    return render_template("index.html", title='Explore', posts=posts.items)
