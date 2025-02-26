32e4055ee0c5190c9d54f6e3221a5ffe0e19d8f1
varetoo_portatil <alvaromi2005@gmail.com>
2025-02-26 13:59:38 +0100

    Add new ADMIN

--- config.py
+++ config.py
@@ -11,4 +11,4 @@ class Config:
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    ADMINS = ['minvdev@gmail.com']
+    ADMINS = ['alvaromi2005@gmail.com','minvdev@gmail.com']
