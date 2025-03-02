d2c1031210eabfef093e90334a8a6a88b40b3538
Varetoo <alvaromi2005@gmail.com>
2025-03-02 13:49:57 +0100

    Syntax fix /email

--- app/email.py
+++ app/email.py
@@ -15,5 +15,5 @@ def send_password_reset_email(user):
                recipients=[user.email],
                text_body=render_template('email/reset_password.txt',
                                          user=user, token=token),
-               html_body=render_template('emaail/reset_password.html',
+               html_body=render_template('email/reset_password.html',
                                          user=user, token=token))
