8f2b83a8e327d282660bd7f3379c0e8416f2203c
Varetoo <alvaromi2005@gmail.com>
2025-03-27 14:00:31 +0100

    Import lazy_gettext ' _l() ' /__init__

--- app/__init__.py
+++ app/__init__.py
@@ -7,7 +7,7 @@ from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_mail import Mail
 from flask_moment import Moment
-from flask_babel import Babel
+from flask_babel import Babel, lazy_gettext as _l
 from config import Config
 
 def get_locale():
