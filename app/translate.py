a46714d48cb7787503cde69fb5014d6b6c80416e
Varetoo <alvaromi2005@gmail.com>
2025-03-31 20:52:46 +0200

    Text translation function /translate

---
+++ app/translate.py
@@ -0,0 +1,19 @@
+import requests
+from flask_babel import _
+from app import app
+
+def translate(text, source_language, dest_language):
+    if 'TRANSLATOR_KEY' not in app.config or \
+        not app.config['TRANSLATOR_KEY']:
+            return _('Error: the translation service is not configured.')
+    url = 'https://translation.googleapis.com/language/translate/v2'
+    params = {
+        'key': app.config,
+        'q': text,
+        'source': source_language,
+        'target': dest_language
+    }
+    r = requests.post(url=url, params=params)
+    if r.status_code != 200:
+        return _('Error: the translation service failed.')
+    return r.json()['data']['translations'][0]['translatedText']
