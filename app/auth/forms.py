c6576003c822e04d2d4212c5cb08db720262a08d
Varetoo <alvaromi2005@gmail.com>
2025-04-02 16:16:49 +0200

    Refactoring (auth)

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
 from flask_babel import _, lazy_gettext as _l
-from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField
-from wtforms.validators import DataRequired, Email, EqualTo, ValidationError, Length
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired, Email, EqualTo, ValidationError
 from app import db
 import sqlalchemy as sa
 from app.models import User
@@ -31,30 +31,6 @@ class RegistrationForm(FlaskForm):
             sa.select(User).where(User.email == email.data))
         if user is not None:
             raise ValidationError(_l('Please use a different email adress.'))
-
-class EditProfileForm(FlaskForm):
-    username = StringField(_l('Username'), validators=[DataRequired()])
-    about_me = TextAreaField(_l('About me'), validators=[Length(min=0, max=140)])
-    submit = SubmitField(_l('Submit'))
-    
-    def __init__(self, original_username, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.original_username = original_username
-    
-    def validate_username(self, username):
-        if username.data != self.original_username:
-            user = db.session.scalar(
-                sa.select(User).where(User.username == username.data))
-            if user is not None:
-                raise ValidationError(_l('Please use a different username.'))
-
-class EmptyForm(FlaskForm):
-    submit = SubmitField(_l('Submit'))
-
-class PostForm(FlaskForm):
-    post = TextAreaField(_l('Say something'), validators=[
-        DataRequired(), Length(min=1, max=140)])
-    submit = SubmitField(_l('Submit'))
 
 class ResetPasswordRequestForm(FlaskForm):
     email = StringField(_l('Email'), validators=[DataRequired(), Email()])
