ce1bff4c2bad053d9750ac3fc9440da1ae30e959
Varetoo <alvaromi2005@gmail.com>
2025-04-08 14:26:37 +0200

    Search view function /main/routes

--- app/main/routes.py
+++ app/main/routes.py
@@ -60,6 +60,23 @@ def explore():
     return render_template("index.html", title=_('Explore'), posts=posts.items,
                            next_url=next_url, prev_url=prev_url)
 
+@bp.route('/search')
+@login_required
+def search():
+    if not g.search_form.validate():
+        return redirect(url_for('main.explore'))
+    page = request.args.get('page', 1, type=int)
+    posts, total = Post.search(g.search_form.q.data, page,
+                current_app.config['POSTS_PER_PAGE'])
+    next_url = url_for('main.search', q=g.search_form.q.data, page=page + 1) \
+        if total > page * current_app.config['POSTS_PER_PAGE'] else None
+    
+    prev_url = url_for('main.search', q=g.search_form.q.data, page=page - 1) \
+        if page > 1 else None
+    
+    return render_template('search.html', title=_('Search'), posts=posts,
+                           next_url=next_url, prev_url=prev_url)
+
 @bp.route('/user/<username>')
 @login_required
 def user(username):
