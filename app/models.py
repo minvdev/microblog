5c2ea3132539946a8029c9b3ac0c7c791dd86a9a
Varetoo <alvaromi2005@gmail.com>
2025-04-22 13:14:51 +0200

    Message model /Models

--- app/models.py
+++ app/models.py
@@ -191,3 +191,16 @@ class Post(SearchableMixin, db.Model):
     
     def __repr__(self):
         return '<Post {}>'.format(self.body)
+
+class Message(db.Model):
+    id: so.Mapped[int] = so.mapped_column(primary_key=True)
+    sender_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey(User.id),
+                                                 index=True)
+    recipient_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey(User.id),
+                                                    index=True)
+    body: so.Mapped[str] = so.mapped_column(sa.String(140))
+    timestamp: so.Mapped[datetime] = so.mapped_column(
+        index=True, default=lambda: datetime.now(timezone.utc))
+    
+    def __repr__(self):
+        return '<Message {}>'.format(self.body)
