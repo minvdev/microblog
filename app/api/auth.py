e2793a8e4d4933fc24c2b08fbe3deadf7ad66e73
Varetoo <alvaromi2005@gmail.com>
2025-04-30 16:41:24 +0200

    Token authentication support /api auth

--- app/api/auth.py
+++ app/api/auth.py
@@ -1,10 +1,11 @@
 import sqlalchemy as sa
-from flask_httpauth import HTTPBasicAuth
+from flask_httpauth import HTTPBasicAuth, HTTPTokenAuth
 from app import db
 from app.models import User
 from app.api.errors import error_response
 
 basic_auth = HTTPBasicAuth()
+token_auth = HTTPTokenAuth()
 
 @basic_auth.verify_password
 def verify_password(username, password):
@@ -14,4 +15,12 @@ def verify_password(username, password):
 
 @basic_auth.error_handler
 def basic_auth_error(status):
+    return error_response(status)
+
+@token_auth.verify_token
+def verify_token(token):
+    return User.check_token(token) if token else None
+
+@token_auth.error_handler
+def toekn_auth_error(status):
     return error_response(status)
