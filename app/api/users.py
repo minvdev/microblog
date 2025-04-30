6d6b67d4c4c7b86197cf696d72cca82e36dc96e1
Varetoo <alvaromi2005@gmail.com>
2025-04-30 13:24:21 +0200

    Register a new user /api users

--- app/api/users.py
+++ app/api/users.py
@@ -1,8 +1,9 @@
 from app.api import bp
 from app import db
 from app.models import User
+from flask import request, url_for
+from app.api.errors import bad_request
 import sqlalchemy as sa
-from flask import request
 
 @bp.route('/users/<int:id>', methods=['GET'])
 def get_user(id):
@@ -38,6 +39,21 @@ def get_following(id):
 @bp.route('/users', methods=['POST'])
 def create_user():
     """Register a new user account"""
+    data = request.get_json()
+    if 'username' not in data or 'email' not in data or 'password' not in data:
+        return bad_request('must include username, email and password fields')
+    if db.session.scalar(sa.select(User).where(
+        User.username == data['username'])):
+        return bad_request('please use a different username')
+    if db.session.scalar(sa.select(User).where(
+        User.email == data['email'])):
+        return bad_request('please use a different email address')
+    user = User()
+    user.from_dict(data, new_user=True)
+    db.session.add(user)
+    db.session.commit()
+    return user.to_dict(), 201, {'Location': url_for('api.get_user',
+                                                     id=user.id)}
 
 @bp.route('/users/<int:id>', methods=['PUT'])
 def update_user(id):
