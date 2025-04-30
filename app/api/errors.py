64cf9d16d1a39e2c01c26e9524eea7f9d83f20b9
Varetoo <alvaromi2005@gmail.com>
2025-04-30 12:03:45 +0200

    Error response /api errors

--- app/api/errors.py
+++ app/api/errors.py
@@ -1,2 +1,7 @@
-def bad_request():
-    pass
+from werkzeug.http import HTTP_STATUS_CODES
+
+def error_response(status_code, message=None):
+    payload = {'error': HTTP_STATUS_CODES.get(status_code, 'Unknown error')}
+    if message:
+        payload['message'] = message
+    return payload, status_code
