ebe0dc890c51aa88793218a2a4a8553c45ac50e7
Varetoo <alvaromi2005@gmail.com>
2025-04-30 15:56:15 +0200

    Basic authentication support /api auth

---
+++ app/api/auth.py
@@ -0,0 +1,17 @@
+import sqlalchemy as sa
+from flask_httpauth import HTTPBasicAuth
+from app import db
+from app.models import User
+from app.api.errors import error_response
+
+basic_auth = HTTPBasicAuth()
+
+@basic_auth.verify_password
+def verify_password(username, password):
+    user = db.session.scalar(sa.select(User).where(User.username == username))
+    if user and user.check_password(password):
+        return user
+
+@basic_auth.error_handler
+def basic_auth_error(status):
+    return error_response(status)
