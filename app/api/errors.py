258840c6d1416ddc238489514abfc40272c75c53
Varetoo <alvaromi2005@gmail.com>
2025-04-30 12:31:28 +0200

    Catch all HTTP API errors /api errors

--- app/api/errors.py
+++ app/api/errors.py
@@ -1,4 +1,6 @@
 from werkzeug.http import HTTP_STATUS_CODES
+from werkzeug.exceptions import HTTPException
+from app.api import bp
 
 def error_response(status_code, message=None):
     payload = {'error': HTTP_STATUS_CODES.get(status_code, 'Unknown error')}
@@ -8,3 +10,7 @@ def error_response(status_code, message=None):
 
 def bad_request(message):
     return error_response(400, message)
+
+@bp.errorhandler(HTTPException)
+def handle_exception(e):
+    return error_response(e.code)
