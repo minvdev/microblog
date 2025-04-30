e52a92b6cb24db71aa6aee6168bedabf651f19a9
Varetoo <alvaromi2005@gmail.com>
2025-04-30 11:02:50 +0200

    Paginated representation mixin class /models

--- app/models.py
+++ app/models.py
@@ -60,6 +60,30 @@ class SearchableMixin(object):
 db.event.listen(db.session, 'before_commit', SearchableMixin.before_commit)
 db.event.listen(db.session, 'after_commit', SearchableMixin.after_commit)
 
+class PaginatedAPIMixin(object):
+    @staticmethod
+    def to_collection_dict(query, page, per_page, endpoint, **kwargs):
+        resources = db.paginate(query, page=page, per_page=per_page,
+                                error_out=False)
+        data = {
+            "items": [item.to_dict() for item in resources.items],
+            "_meta": {
+                "page": page,
+                "per_page": per_page,
+                "total_pages": resources.pages,
+                "total_items": resources.total
+            },
+            "_links": {
+                "self": url_for(endpoint, page=page, per_page=per_page,
+                                **kwargs),
+                "next": url_for(endpoint, page=page + 1, per_page=per_page,
+                                **kwargs) if resources.has_next else None,
+                "prev": url_for(endpoint, page=page - 1, per_page=per_page,
+                                **kwargs) if resources.has_prev else None
+            }
+        }
+        return data
+
 followers = sa.Table(
     'followers',
     db.metadata,
