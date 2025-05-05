50a6ce8f1f8e9cac736d7d29f51f1680c57eba4c
Varetoo <alvaromi2005@gmail.com>
2025-05-05 18:07:41 +0200

    Complete get_posts route of the API /api users

--- app/api/users.py
+++ app/api/users.py
@@ -43,13 +43,17 @@ def get_following(id):
 
 @bp.route('/users/<int:id>/posts', methods=['GET'])
 @token_auth.login_required
-def new_post(id):
-    """Return all the posts of the user""" 
+def get_posts(id):
+    """Return all the posts of the user"""
+    page = request.args.get('page', 1, type=int)
+    per_page = min(request.args.get('per_page', 10, type=int), 100)
+    return Post.to_collection_dict(sa.select(Post).where(Post.user_id == id), page, per_page, 'api.get_posts', id=id)
 
 @bp.route('/users', methods=['POST'])
 def create_user():
     """Register a new user account"""
     data = request.get_json()
+    
     if 'username' not in data or 'email' not in data or 'password' not in data:
         return bad_request('must include username, email and password fields')
     if db.session.scalar(sa.select(User).where(
