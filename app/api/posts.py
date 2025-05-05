46016c6559d78fcf58917fd34d7ce280a7230567
Varetoo <alvaromi2005@gmail.com>
2025-05-05 18:33:35 +0200

    Complete create_post route of the API /api posts

--- app/api/posts.py
+++ app/api/posts.py
@@ -3,7 +3,9 @@ from app.api.auth import token_auth
 from app import db
 from app.models import Post
 import sqlalchemy as sa
-from flask import request
+from flask import request, url_for, abort
+from app.api.errors import bad_request
+from langdetect import detect, LangDetectException
 
 @bp.route('/posts/<int:id>', methods=['GET'])
 @token_auth.login_required
@@ -24,3 +26,24 @@ def get_posts():
 @token_auth.login_required
 def create_post():
     """Share a new post"""
+    data = request.get_json()
+    if 'body' not in data:
+        return bad_request('Must include body field')
+    
+    if len(data['body']) > 140:
+        return bad_request('Body cannot exceed 140 characters')
+    
+    data['user_id'] = token_auth.current_user.id if token_auth.current_user.id \
+        else bad_request('The user id could not be found')
+    
+    try:
+        data['language'] = detect(data['body'])
+    except LangDetectException:
+        data['language'] = ''
+    
+    post = Post()
+    post.from_dict(data)
+    db.session.add(post)
+    db.session.commit()
+    return post.to_dict(), 201, {'Location': url_for('api.get_post',
+                                                     id=post.id)}
