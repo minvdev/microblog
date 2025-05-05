78dc4bbb2f8b613b7cb560d6e482b2990c884e70
Varetoo <alvaromi2005@gmail.com>
2025-05-05 18:16:04 +0200

    Complete get_posts route of the API /api posts

--- app/api/posts.py
+++ app/api/posts.py
@@ -2,6 +2,8 @@ from app.api import bp
 from app.api.auth import token_auth
 from app import db
 from app.models import Post
+import sqlalchemy as sa
+from flask import request
 
 @bp.route('/posts/<int:id>', methods=['GET'])
 @token_auth.login_required
@@ -13,6 +15,10 @@ def get_post(id):
 @token_auth.login_required
 def get_posts():
     """Return all posts from the db"""
+    page = request.args.get('page', 1, type=int)
+    per_page = min(request.args.get('per_page', 10, type=int), 100)
+    return Post.to_collection_dict(sa.select(Post), page, per_page,
+                                   'api.get_posts')
 
 @bp.route('/posts', methods=['POST'])
 @token_auth.login_required
