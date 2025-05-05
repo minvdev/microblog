dbb9f3251f26a1dbfd305823903bb82124532ba7
Varetoo <alvaromi2005@gmail.com>
2025-05-05 18:11:00 +0200

    Complete get_post route of the API /api posts

--- app/api/posts.py
+++ app/api/posts.py
@@ -1,10 +1,13 @@
 from app.api import bp
 from app.api.auth import token_auth
+from app import db
+from app.models import Post
 
-@bp.route('/posts/<id>', methods=['GET'])
+@bp.route('/posts/<int:id>', methods=['GET'])
 @token_auth.login_required
 def get_post(id):
     """Return a post from an id"""
+    return db.get_or_404(Post, id).to_dict()
 
 @bp.route('/posts', methods=['GET'])
 @token_auth.login_required
